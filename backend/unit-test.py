11b8a9223be9fae8de1f41f25d2103e6cc15b8bd
Himanshu Chauhan <54837598+himanshu1365@users.noreply.github.com>
2020-05-28 07:49:00 +0530

    Merge pull request #4 from himanshu1365/develop
    added commission api and testcase file to develop

--- backend/unit-test.py
+++ backend/unit-test.py
@@ -108,5 +108,63 @@ class PocTest():
 	def customer_history(self):
 		data=requests.get('http://127.0.0.1:5000/api/customer/history?phoneNo=8054544097&pageIndex=0&pageSize=5')
 		return data.json()
+
+class CommissionData():
+    def __init__(self):
+        self.commission_details = {
+            "class": "4929",
+            "sku": "9167",
+            "retail": "190.78",
+            "mkdn": "58.37",
+            "quantity": "1",
+            "desc": "rhoncus mauris enim",
+            "commissionType": "MTR1",
+            "amount": "15.55",
+            "employeePercentage": "56",
+            "salePercentage": "100",
+            "commEmpId": "MKT10",
+            "employeeCommissionType": "SALECNS"
+        }
+
+    def commission_details_list(self):
+        return self.commission_details
+
+    def commission_detail_error(self):
+        return "No commission information for this transaction Id"
+
+
+class CreditCardData():
+    def __init__(self):
+        self.credit_card_details = {
+            "sent": "02:52:29pm",
+            "rcvd": "03:28:16pm",
+            "done": "04:59:30pm",
+            "type": "BCRD",
+            "entry": "RENTR",
+            "auth": "AUTO APPROVED"
+        }
+
+
+class CommissionTestCases(unittest.TestCase):
+    def test_commission_list(self):
+        obj = CommissionData()
+        response = requests.get("http://127.0.0.1:5000/commission/4830*35*1672")
+        data = response.json()['commissionList']
+        self.assertEqual(data[0],obj.commission_details_list())
+    
+    def test_commission_data(self):
+        obj = CommissionData()
+        response = requests.get("http://127.0.0.1:5000/commission/5823*83*2072")
+        data = response.json()['error']
+        self.assertEqual(data,obj.commission_detail_error())
+
+
+    def test_credit_card_details(self):
+        obj = CreditCardData()
+        response = requests.get("http://127.0.0.1:5000/transaction/4830*35*1672/creditCard/authentication")
+        data = response.json()['cardDetails'][0]
+        print(data)
+        self.assertEqual(data,obj.credit_card_details)
+
 if __name__ == '__main__':
     unittest.main()
