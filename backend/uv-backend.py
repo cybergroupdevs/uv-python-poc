11ae333e3dd2083d03c33ff0ada1c41fd0545b76
ashishrawat0 <ashish1198.cse16@chitkara.edu.in>
2020-05-29 16:06:35 +0530

    change in error response

--- backend/uv-backend.py
+++ backend/uv-backend.py
@@ -211,10 +211,16 @@ def customer_details():
     customer_id=request.args.get('customerId')
     customer_file= u2py.File("CUSTOMERS")
     data=[]
+    error={}
     cmd=f"LIST PHONE.NO F.NAME L.NAME ADDRESS CITY ZIP.CODE PHONE.LONG PFID DATA {customer_id} CUSTOMERS TOJSON"
     details=u2py.Command(cmd).run(capture=True)
     if "not found."in details:
-        data.append(f"{customer_id}, not found")
+        error["msg"]=f"{customer_id}, not found"
+        data.append(error)
+        return Response(
+            json.dumps(data),
+            status=404,
+            mimetype='application/json')
     else:
         details=json.loads(details)
         del details['CUSTOMERS'][0]["_ID"]
@@ -222,88 +228,87 @@ def customer_details():
         keys=["phoneNo","firstName","lastName","address","city","zipCode","altPhoneNo","pfid"]
         customer_dict={key: value for key, value in zip(keys, values)}
         data.append(customer_dict)
-    return Response(
-    	json.dumps(data),
-    	status=200,
-    	mimetype='application/json')
+        return Response(
+    	    json.dumps(data),
+    	    status=200,
+    	    mimetype='application/json')
 ########################
 #### CONSULTANT API ####
 ########################
 @app.route('/api/consultant',methods=['GET'])
 def consultant_details():
-	transaction_id=request.args.get('transactionId')
-	transaction_file=u2py.File("TRANSACTION")
-	transaction_cmd=f"LIST TRANS.NO PHONE CommEmplId CommEmplType FITTER DATA {transaction_id} TRANSACTION TOJSON"
-	transaction_details=u2py.Command(transaction_cmd).run(capture=True)
-	transaction_details=json.loads(transaction_details)
-	trans_no=transaction_details['TRANSACTION'][0]["TRANS.NO"]
-	phone_no=transaction_details['TRANSACTION'][0]["PHONE"]
-	em_file=u2py.File("EM")
-	details={}
-	data=[]
-	consultant_cmd=f"LIST FNAME LNAME SHORTNAME NICKNAME DATA {phone_no} EM TOJSON"
-	consultant_details=u2py.Command(consultant_cmd).run(capture=True)
-	consultant_details=json.loads(consultant_details)
-	if(("NICKNAME",consultant_details['EM'][0])==True):
-		business_name=consultant_details['EM'][0]["NICKNAME"]
-	else:
-		business_name=""
-	first_name=consultant_details['EM'][0]["FNAME"]
-	last_name=consultant_details['EM'][0]["LNAME"]
-	short_name=consultant_details['EM'][0]["SHORTNAME"]
+    transaction_id=request.args.get('transactionId')
+    transaction_file=u2py.File("TRANSACTION")
+    transaction_cmd=f"LIST TRANS.NO PHONE CommEmplId CommEmplType FITTER DATA {transaction_id} TRANSACTION TOJSON"
+    transaction_details=u2py.Command(transaction_cmd).run(capture=True)
+    transaction_details=json.loads(transaction_details)
+    trans_no=transaction_details['TRANSACTION'][0]["TRANS.NO"]
+    phone_no=transaction_details['TRANSACTION'][0]["PHONE"]
+    em_file=u2py.File("EM")
+    details={}
+    data=[]
+    consultant_cmd=f"LIST FNAME LNAME SHORTNAME NICKNAME DATA {phone_no} EM TOJSON"
+    consultant_details=u2py.Command(consultant_cmd).run(capture=True)
+    consultant_details=json.loads(consultant_details)
+    if(("NICKNAME",consultant_details['EM'][0])==True):
+        business_name=consultant_details['EM'][0]["NICKNAME"]
+    else:
+        business_name=""
+    first_name=consultant_details['EM'][0]["FNAME"]
+    last_name=consultant_details['EM'][0]["LNAME"]
+    short_name=consultant_details['EM'][0]["SHORTNAME"]
         ####checks the businessName if present it replaces the firstname
-	if(business_name!=''):
-		operator=str(business_name)
-	else:
-		operator=str(first_name)
-	operator=operator+" "+str(last_name)
-	if(len(operator)+len(short_name)<23):
-		operator=operator+" ("+str(short_name)+")"
-	details["operator"]=operator
+    if(business_name!=''):
+        operator=str(business_name)
+    else:
+        operator=str(first_name)
+    operator=operator+" "+str(last_name)
+    if(len(operator)+len(short_name)<23):
+        operator=operator+" ("+str(short_name)+")"
+    details["operator"]=operator
 	####they only want length till 23(used for extra precaution)
-	operator=str(operator[0:23])
-	if(key_exists('CommEmplType',transaction_details['TRANSACTION'][0]["ITEM_MV"][0])):
-		employee_type=transaction_details['TRANSACTION'][0]["ITEM_MV"][0]['CommEmplType']
-	else:
-		employee_type=''
-	if employee_type =='':
-		employee_type="SLS CONSULT"
-	else:
-		employee_type=employee_type.split("*")
-		employee_type=str(employee_type[2])
-	if(key_exists('CommEmplId',transaction_details['TRANSACTION'][0]["ITEM_MV"][0])):
-		employee_id=transaction_details['TRANSACTION'][0]["ITEM_MV"][0]['CommEmplId']
-	else:
-		employee_id=""
-	if(check_existing_record("EM",employee_id)):
+    operator=str(operator[0:23])
+    if(key_exists('CommEmplType',transaction_details['TRANSACTION'][0]["ITEM_MV"][0])):
+        employee_type=transaction_details['TRANSACTION'][0]["ITEM_MV"][0]['CommEmplType']
+    else:
+        employee_type=''
+    if employee_type =='':
+        employee_type="SLS CONSULT"
+    else:
+        employee_type=employee_type.split("*")
+        employee_type=str(employee_type[2])
+    if(key_exists('CommEmplId',transaction_details['TRANSACTION'][0]["ITEM_MV"][0])):
+        employee_id=transaction_details['TRANSACTION'][0]["ITEM_MV"][0]['CommEmplId']
+    else:
+        employee_id=""
+    if(check_existing_record("EM",employee_id)):
 	####sets name to Noconsultant if record not found
-		employee_cmd=f"LIST FNAME LNAME SHORTNAME DATA {employee_id} EM TOJSON"
-		employee_details=u2py.Command(employee_cmd).run(capture=True)
-		employee_details=json.loads(employee_details)
-		employee_first_name=employee_details['EM'][0]["FNAME"]
-		employee_last_name=employee_details['EM'][0]["LNAME"]
-		employee_short_name=employee_details['EM'][0]["SHORTNAME"]
-		details[employee_type]=str(employee_first_name)+" "+str(employee_last_name)+" ("+str(employee_short_name)+")"
-	else:
-		details[employee_type]="No consultant"
+        employee_cmd=f"LIST FNAME LNAME SHORTNAME DATA {employee_id} EM TOJSON"
+        employee_details=u2py.Command(employee_cmd).run(capture=True)
+        employee_details=json.loads(employee_details)
+        employee_first_name=employee_details['EM'][0]["FNAME"]
+        employee_last_name=employee_details['EM'][0]["LNAME"]
+        employee_short_name=employee_details['EM'][0]["SHORTNAME"]
+        details[employee_type]=f"{employee_file_name} {employee_last_name} ({employee_short_name})"
+    else:
+        details[employee_type]="No consultant"
         ####add name from em file where recordId is we get from record<248> of transaction
-	if(key_exists("FITTER",transaction_details['TRANSACTION'][0])):
-		fitter_id=transaction_details['TRANSACTION'][0]["FITTER"]
-		if(fitter_id!=""):
-			fitter_cmd=f"LIST FNAME LNAME SHORTNAME DATA {fitter_id} EM TOJSON"
-			fitter_details=u2py.Command(fitter_cmd).run(capture=True)
-			fitter_details=json.loads(fitter_details)
-			fitter_first_name=fitter_details['EM'][0]["FNAME"]
-			fitter_last_name=fitter_details['EM'][0]["LNAME"]
-			fitter_short_name=fitter_details['EM'][0]["SHORTNAME"]
-			fitter_name=str(fitter_first_name)+" "+str(fitter_last_name)+" ("+str(fitter_short_name)+")"
-		else:
-			fitter_name=fitter_id
-	if(key_exists("FITTER",transaction_details['TRANSACTION'][0]) and fitter_id!=""):
-	####only adds if there is a fitterId(doubt why they set its value null if not sending)
-		details["SRC ASSOC"]=fitter_name
-	data.append(details)
-	return Response(
+    if(key_exists("FITTER",transaction_details['TRANSACTION'][0])):
+        fitter_id=transaction_details['TRANSACTION'][0]["FITTER"]
+        if(fitter_id!=""):
+            fitter_cmd=f"LIST FNAME LNAME SHORTNAME DATA {fitter_id} EM TOJSON"
+            fitter_details=u2py.Command(fitter_cmd).run(capture=True)
+            fitter_details=json.loads(fitter_details)
+            fitter_first_name=fitter_details['EM'][0]["FNAME"]
+            fitter_last_name=fitter_details['EM'][0]["LNAME"]
+            fitter_short_name=fitter_details['EM'][0]["SHORTNAME"]
+            fitter_name=str(fitter_first_name)+" "+str(fitter_last_name)+" ("+str(fitter_short_name)+")"
+            details["SRC ASSOC"]=fitter_name
+        else:
+            fitter_name=fitter_id
+	####only adds if there is a fitterId(doubt why they set its value null if not sending
+    data.append(details)
+    return Response(
 		json.dumps(data),
 		status=200,
 		mimetype="application/json"
