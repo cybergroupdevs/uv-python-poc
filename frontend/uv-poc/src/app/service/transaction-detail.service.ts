df3fda3da0d6a5ce1b9c20656a964ae5baf5fefd
lakshyakalia <lkalia.cse16@chitkarauniversity.edu.in>
2020-06-06 01:51:12 +0530

    Made home component Closes #18

--- frontend/uv-poc/src/app/service/transaction-detail.service.ts
+++ frontend/uv-poc/src/app/service/transaction-detail.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Router, NavigationExtras } from '@angular/router';
+import { Router } from '@angular/router';
 @Injectable({
   providedIn: 'root'
 })
