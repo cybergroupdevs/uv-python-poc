65755ecd812cf37135cb2bed90fb0779440a595e
himanshu1365 <himanshu1365.cse16@chitkara.edu.in>
2020-06-05 16:53:43 +0530

    Closes #16

--- frontend/uv-poc/src/app/app.module.ts
+++ frontend/uv-poc/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommissionDetailsComponent } from './commission-details/commission-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommissionDetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
