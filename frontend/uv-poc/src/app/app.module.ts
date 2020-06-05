93a18a7baa14e55d9b1d78e40b13f044c7abd404
ashishrawat0 <ashish1198.cse16@chitkara.edu.in>
2020-06-05 16:31:33 +0530

    created consultant details component

--- frontend/uv-poc/src/app/app.module.ts
+++ frontend/uv-poc/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ConsultantDetailsComponent } from './consultant-details/consultant-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConsultantDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/uv-poc/src/app/consultant-details/consultant-details.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consultant-details',
+  template: `
+    <p>
+      consultant-details works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ConsultantDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
