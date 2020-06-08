ed37a4d48a5ba943d5fcead72f7e24357b5785b4
lakshyakalia <lkalia.cse16@chitkarauniversity.edu.in>
2020-06-08 19:34:49 +0530

    transaction details component added

--- frontend/uv-poc/src/app/app-routing.module.ts
+++ frontend/uv-poc/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TransactionDetailComponent } from '../app/transaction-detail/transaction-detail.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'transaction', component: TransactionDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ frontend/uv-poc/src/app/transaction-detail/transaction-detail.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-detail',
+  template: `
+    <p>
+      transaction-detail works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class TransactionDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
