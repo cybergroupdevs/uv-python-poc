1ed3ffb7ead275b951d8f823d367e31035032163
lakshyakalia <lkalia.cse16@chitkarauniversity.edu.in>
2020-06-08 16:40:47 +0530

    Made Transaction Detail component Closes #25

--- frontend/uv-poc/src/app/app-routing.module.ts
+++ frontend/uv-poc/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TransactionDetailComponent } from '../app/transaction-detail/transaction-detail.component'
+import { HomeComponent } from './home/home.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component : HomeComponent },
+  { path: 'transaction', component : TransactionDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/uv-poc/src/app/app.module.ts
+++ frontend/uv-poc/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
-import { ReactiveFormsModule } from '@angular/forms'
+import { ReactiveFormsModule } from '@angular/forms';
+import { TransactionDetailComponent } from './transaction-detail/transaction-detail.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TransactionDetailComponent
   ],
   imports: [
     BrowserModule,
