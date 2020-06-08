5aa7fb3b2e6cba08711737bff808da197765f469
lakshyakalia <lkalia.cse16@chitkarauniversity.edu.in>
2020-06-08 19:46:38 +0530

    Made transaction detail component Closes #25

--- frontend/uv-poc/src/app/app.module.ts
+++ frontend/uv-poc/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,7 +14,8 @@ import { TransactionDetailComponent } from './transaction-detail/transaction-det
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
