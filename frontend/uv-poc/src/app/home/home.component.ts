1ce06646b572c8b618e000a0dae8168d3c7ec8a3
lakshyakalia <lkalia.cse16@chitkarauniversity.edu.in>
2020-06-10 00:39:18 +0530

    Bootstrap added and CSS removed

--- frontend/uv-poc/src/app/home/home.component.ts
+++ frontend/uv-poc/src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Router, NavigationExtras } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+  styleUrls: [],
 })
 export class HomeComponent implements OnInit {
   constructor(
