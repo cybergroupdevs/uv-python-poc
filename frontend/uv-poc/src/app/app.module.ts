f64eaac07874868b1a423e8452b6fa8b83aff47e
lakshyakalia <lkalia.cse16@chitkarauniversity.edu.in>
2020-06-09 17:04:55 +0530

    formatting changes

--- frontend/uv-poc/src/app/app.module.ts
+++ frontend/uv-poc/src/app/app.module.ts
@@ -1,28 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
-import { ReactiveFormsModule } from '@angular/forms'
-import { ConsultantDetailsComponent } from "./consultant-details/consultant-details.component";
+import { ReactiveFormsModule } from '@angular/forms';
+import { ConsultantDetailsComponent } from './consultant-details/consultant-details.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    ConsultantDetailsComponent
-  ],
+  declarations: [AppComponent, HomeComponent, ConsultantDetailsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
