44fed969be385837c3789e49c66283bcc76f1bd9
Ashish Rawat <54837314+ashishrawat0@users.noreply.github.com>
2020-06-10 07:45:48 +0530

    customerHistory Closes #30 (#32)
    * customerHistory Closes #30
    * fixed symmetry in frontend code
    * changed http param method

--- frontend/uv-poc/src/app/app.module.ts
+++ frontend/uv-poc/src/app/app.module.ts
@@ -1,17 +1,28 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { ConsultantDetailsComponent } from "./consultant-details/consultant-details.component";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CustomerHistoryComponent } from './customer-history/customer-history.component';
+import { ConsultantDetailsComponent } from './consultant-details/consultant-details.component';
 @NgModule({
-  declarations: [AppComponent, ConsultantDetailsComponent],
+  declarations: [
+    AppComponent,
+    ConsultantDetailsComponent,
+    CustomerHistoryComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatDialogModule,
     HttpClientModule,
   ],
   providers: [],
