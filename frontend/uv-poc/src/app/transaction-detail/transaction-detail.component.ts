586ceaa925112e75cc2a80c058ce5f8a14736383
ashishrawat0 <ashish1198.cse16@chitkara.edu.in>
2020-06-15 15:06:35 +0530

    connected all modules

--- frontend/uv-poc/src/app/transaction-detail/transaction-detail.component.ts
+++ frontend/uv-poc/src/app/transaction-detail/transaction-detail.component.ts
@@ -4,6 +4,10 @@ import { CreditCardService } from '../service/credit-card.service';
 import { DiscountService } from '../service/discount.service';
 import { RefundService } from '../service/refund.service';
 import { CommissionService } from '../service/commission.service';
+import { CustomerHistoryComponent } from '../customer-history/customer-history.component';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CustomerService } from '../service/customer.service';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'transaction-detail',
@@ -16,8 +20,16 @@ export class TransactionDetailComponent implements OnInit {
     private creditCardService: CreditCardService,
     private discountService: DiscountService,
     private refundService: RefundService,
-    private commissionService: CommissionService
+    private commissionService: CommissionService,
+    private customerService: CustomerService,
+    public dialog: MatDialog
   ) {}
+  customerData = [];
+  phone: string;
+  count: Number;
+  phoneNoForm = new FormGroup({
+    phoneNo: new FormControl('', [Validators.required]),
+  });
   headerData;
   cardDetails: any;
   creditCardHeading = [];
@@ -64,5 +76,24 @@ export class TransactionDetailComponent implements OnInit {
 
   onChangeTab(event) {
     this.commissionService.changeActiveTab(event['tab']['textLabel']);
+  }
+  openDialog(): void {
+    let pageIndex = 0;
+    let pageSize = 5;
+    this.phone = this.phoneNoForm.value['phoneNo'];
+    this.customerService
+      .list(this.phone, pageIndex, pageSize)
+      .subscribe((res: any) => {
+        this.customerData = res['customerHistory'];
+        this.count = res['count'];
+        const dialogRef = this.dialog.open(CustomerHistoryComponent, {
+          width: '800px',
+          data: {
+            customerData: this.customerData,
+            phoneNo: this.phone,
+            count: this.count,
+          },
+        });
+      });
   }
 }
