818347b315d97cfb47f79bfd9d9b605d6ba421e5
himanshu1365 <himanshu1365.cse16@chitkara.edu.in>
2020-06-15 14:20:19 +0530

    integrated commission screen in transaction details and updated html file

--- frontend/uv-poc/src/app/transaction-detail/transaction-detail.component.ts
+++ frontend/uv-poc/src/app/transaction-detail/transaction-detail.component.ts
@@ -3,20 +3,21 @@ import { Component, OnInit } from '@angular/core';
 import { CreditCardService } from '../service/credit-card.service';
 import { DiscountService } from '../service/discount.service';
 import { RefundService } from '../service/refund.service';
+import { CommissionService } from '../service/commission.service';
 
 @Component({
   selector: 'transaction-detail',
   templateUrl: './transaction-detail.component.html',
   styleUrls: ['./transaction-detail.component.css'],
 })
-
 export class TransactionDetailComponent implements OnInit {
   constructor(
     private activatedRoute: ActivatedRoute,
     private creditCardService: CreditCardService,
     private discountService: DiscountService,
-    private refundService: RefundService
-  ) { }
+    private refundService: RefundService,
+    private commissionService: CommissionService
+  ) {}
   headerData;
   cardDetails: any;
   creditCardHeading = [];
@@ -30,19 +31,22 @@ export class TransactionDetailComponent implements OnInit {
     this.activatedRoute.queryParams.subscribe((params) => {
       this.headerData = JSON.parse(params['transactionData']);
     });
-  }
-  showCreditCardDetails() {
-    this.creditCardService
-      .get(this.headerData.customerDetails['transactionId'])
-      .subscribe((res: any) => {
-        this.cardDetails = res['cardDetails'];
-        this.creditCardHeading = Object.keys(res['cardDetails'][0]);
-      });
+
+    this.commissionService.activeTab.subscribe((data) => {
+      if (data.toString() == 'Credit card') {
+        this.creditCardService
+          .get(this.headerData.customerDetails['transactionId'])
+          .subscribe((res: any) => {
+            this.cardDetails = res['cardDetails'];
+            this.creditCardHeading = Object.keys(res['cardDetails'][0]);
+          });
+      }
+    });
   }
 
   showRefundDetails() {
     this.refundService
-      .refundDetails(this.transactionId)
+      .refundDetails(this.headerData.customerDetails['transactionId'])
       .subscribe((res: any) => {
         this.refundManagerName = res.refundData['refundMgrName'];
         this.refundTicketNumber = res.refundData['ticketNumber'];
@@ -50,9 +54,15 @@ export class TransactionDetailComponent implements OnInit {
   }
 
   showDiscountDetails() {
-    this.discountService.get(this.transactionId).subscribe((res: any) => {
-      this.discountPct = res.discountDetails['pct'];
-      this.discountSubtotal = res.discountDetails['subTotal'];
-    });
+    this.discountService
+      .get(this.headerData.customerDetails['transactionId'])
+      .subscribe((res: any) => {
+        this.discountPct = res.discountDetails['pct'];
+        this.discountSubtotal = res.discountDetails['subTotal'];
+      });
+  }
+
+  onChangeTab(event) {
+    this.commissionService.changeActiveTab(event['tab']['textLabel']);
   }
 }
